ef4596ef337b5ca58d0c6616b8d73adb3b364372
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2023-10-08 15:48:57 +0200

    [ADD] props

--- my_pokedex/src/components/PokemonCard.jsx
+++ my_pokedex/src/components/PokemonCard.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
-
-function PokemonCard() {
-    const pokemon = pokemonList[0];
+function PokemonCard(props) {
+    console.log(props)
+    const { pokemon } = props;
     if (pokemon.imgSrc) {
         return (
             <figure><img src={pokemon.imgSrc} alt={pokemon.name}></img>
